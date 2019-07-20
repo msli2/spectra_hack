bff74f93ef95bf92067c4e137523d722828e3e6e
msli2 <msli2@illinois.edu>
2019-07-20 16:24:50 -0700

    added main activity and select work time

--- app/src/main/java/com/example/mirandali/spectra_hack/MainActivity.java
+++ app/src/main/java/com/example/mirandali/spectra_hack/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.mirandali.spectra_hack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_begin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_begin = (Button) findViewById(R.id.button_begin);
+
+        button_begin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getOpenSelectWorkTime();
+            }
+        });
+    }
+
+    public void getOpenSelectWorkTime() {
+        Intent a = new Intent(MainActivity.this, SelectWorkTime.class);
+        startActivity(a);
     }
 }
---
+++ app/src/main/java/com/example/mirandali/spectra_hack/SelectWorkTime.java
@@ -0,0 +1,17 @@
+package com.example.mirandali.spectra_hack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectWorkTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_work_time);
+    }
+
+}
