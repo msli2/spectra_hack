a1c25cb755612e3cb2356732dff0a956a93962aa
msli2 <msli2@illinois.edu>
2019-07-20 18:31:14 -0700

    added buttons

--- app/src/main/java/com/example/mirandali/spectra_hack/BreakTimer.java
+++ app/src/main/java/com/example/mirandali/spectra_hack/BreakTimer.java
@@ -1,4 +1,50 @@
 package com.example.mirandali.spectra_hack;
 
-public class BreakTimer {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BreakTimer extends AppCompatActivity{
+    private Button button_BacktoWork;
+    private Button button_newActivity;
+    private Button button_imDone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.break_timer);
+        button_BacktoWork = (Button) findViewById(R.id.button_BackToWork);
+        button_newActivity = (Button) findViewById(R.id.button_newActivity);
+        button_imDone = (Button) findViewById(R.id.button_imDone);
+
+        button_BacktoWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkTime();
+            }
+        });
+        button_newActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateNewActivity();
+            }
+        });
+        button_imDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doneWithWork();
+            }
+        });
+    }
+    public void WorkTime() {
+        Intent a = new Intent(BreakTimer.this, WorkTimer.class);
+        startActivity(a);
+    }
+    public void generateNewActivity(){
+    }
+    public void doneWithWork() {
+        Intent a = new Intent(BreakTimer.this, Done.class);
+        startActivity(a);
+    }
 }
--- app/src/main/java/com/example/mirandali/spectra_hack/WorkTimer.java
+++ app/src/main/java/com/example/mirandali/spectra_hack/WorkTimer.java
@@ -1,4 +1,49 @@
 package com.example.mirandali.spectra_hack;
 
-public class WorkTimer {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WorkTimer extends AppCompatActivity{
+    private Button button_breakTime;
+    private Button button_Pause;
+    private Button button_imDone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.break_timer);
+        button_breakTime = (Button) findViewById(R.id.button_breakTime);
+        button_Pause = (Button) findViewById(R.id.button_Pause);
+        button_imDone = (Button) findViewById(R.id.button_imDone);
+        button_breakTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itsBreakTime();
+            }
+        });
+        button_Pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause_clock();
+            }
+        });
+        button_imDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doneWithWork();
+            }
+        });
+    }
+    public void itsBreakTime() {
+        Intent a = new Intent(WorkTimer.this, BreakTimer.class);
+        startActivity(a);
+    }
+    public void pause_clock(){
+    }
+    public void doneWithWork() {
+        Intent b = new Intent(WorkTimer.this,Done.class);
+        startActivity(b);
+    }
 }
