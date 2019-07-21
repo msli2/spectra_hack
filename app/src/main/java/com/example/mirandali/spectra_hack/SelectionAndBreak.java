4c7ebc5831dc3757267dedca9a92f5e5d1ea526c
msli2 <msli2@illinois.edu>
2019-07-20 18:46:26 -0700

    fixed buttons

--- app/src/main/java/com/example/mirandali/spectra_hack/SelectionAndBreak.java
+++ app/src/main/java/com/example/mirandali/spectra_hack/SelectionAndBreak.java
@@ -1,5 +1,4 @@
 package com.example.mirandali.spectra_hack;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,36 +6,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class SelectionAndBreak extends AppCompatActivity{
-    private Button button_startWorking;
-    private Button button_back;
+    private Button button_startWork;
+    private Button button_backward;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selection_and_break);
+        button_startWork = (Button) findViewById(R.id.button_startWork);
+        button_backward = (Button) findViewById(R.id.button_backward);
 
-        button_back = (Button) findViewById(R.id.button_back);
-        button_startWorking = (Button) findViewById(R.id.button_startworking);
-
-        button_startWorking.setOnClickListener(new View.OnClickListener() {
+        button_startWork.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getOpenSelectWorkTimeOne();
+                getOpenSelectWorkTime();
             }
         });
 
-        button_back.setOnClickListener(new View.OnClickListener() {
+        button_backward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getOpenSelectWorkTimeTwo();
+                goBack();
             }
         });
     }
-    public void getOpenSelectWorkTimeOne() {
+    public void getOpenSelectWorkTime() {
         Intent a = new Intent(SelectionAndBreak.this, WorkTimer.class);
         startActivity(a);
     }
 
-    public void getOpenSelectWorkTimeTwo() {
+    public void goBack() {
         Intent b = new Intent(SelectionAndBreak.this, SelectWorkTime.class);
         startActivity(b);
     }
